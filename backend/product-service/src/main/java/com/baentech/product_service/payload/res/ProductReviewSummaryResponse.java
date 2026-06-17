430076ae3778916099ebf0670ff8bc2e47af664c
M. Saroni <msaroni454@gmail.com>
2026-06-18 01:33:48 +0700

    Add product review summary response

---
+++ backend/product-service/src/main/java/com/baentech/product_service/payload/res/ProductReviewSummaryResponse.java
@@ -0,0 +1,13 @@
+package com.baentech.product_service.payload.res;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductReviewSummaryResponse {
+
+    private Long productId;
+    private Double averageRating;
+    private Long totalReviews;
+}
