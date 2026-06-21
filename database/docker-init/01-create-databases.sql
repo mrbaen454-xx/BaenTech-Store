2b9c03aa482e82ebb25e2b2d574c4571ec74041e
mrbaen454-xx <msaroni454@gmail.com>
2026-06-21 09:41:59 +0700

    Add docker compose deployment setup

---
+++ database/docker-init/01-create-databases.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE baentech_auth_db;
+CREATE DATABASE baentech_product_db;
+CREATE DATABASE baentech_user_db;
+CREATE DATABASE baentech_cart_db;
+CREATE DATABASE baentech_order_db;
+CREATE DATABASE baentech_payment_db;
+CREATE DATABASE baentech_shipping_db;
